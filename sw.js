33f8e4255bf5edf36df890530cdee6aa033a0f5b
Kunming HKWX <252695800@qq.com>
2022-11-29 20:32:20 +0800

    测试清理缓存资源文件5

--- sw.js
+++ sw.js
@@ -32,44 +32,44 @@ console.log('service worker 版本:', cacheName)
 // }
 // cleanCaches()
 //需要缓存的资源列表
-const cacheFiles = [
-    '/digital-sign/',
-    // './index.html',
-    // './index.js',
-    // './style.css',
-    // './img/wang.jpeg',
-    // './img/loading.svg'
-];
+// const cacheFiles = [
+//     '/digital-sign/',
+//     // './index.html',
+//     // './index.js',
+//     // './style.css',
+//     // './img/wang.jpeg',
+//     // './img/loading.svg'
+// ];
 //监听install事件, 完成安装时， 进行文件缓存
-self.addEventListener('install', function (e) {
-    console.log('service worker 安装成功');
-    const cacheOpenPromise = caches.open(cacheName).then(function (cache) {
-        return cache.addAll(cacheFiles);
-    });
-    e.waitUntil(cacheOpenPromise);
-});
-//监听activite事件， 激活后通过cache的key来判断是否需要更新cache中的静态资源
-self.addEventListener('activate',function (e) {
-    console.log('service worker 激活成功');
-    const cachePromise = caches.keys().then(function (keys) {
-        return Promise.all(keys.map(function (key) {
-            if(key !== cacheName) {
-                return caches.delete(key);
-            }
-        }))
-    });
-    e.waitUntil(cachePromise);
-    return self.clients.claim(); //激活触发Service Worker
-});
-//cache存在则使用cache，无cache则fetch服务器端请求资源
-self.addEventListener('fetch', function (e) {
-    // console.log('service worker 抓取请求: ' + e.request.url)
-    e.respondWith(
-      caches.match(e.request).then(function (cache) {
-          return cache || fetch(e.request);
-      }).catch(function (err) {
-            console.log(err);
-            return fetch(e.request);
-      })
-    );
-});
+// self.addEventListener('install', function (e) {
+//     console.log('service worker 安装成功');
+//     const cacheOpenPromise = caches.open(cacheName).then(function (cache) {
+//         return cache.addAll(cacheFiles);
+//     });
+//     e.waitUntil(cacheOpenPromise);
+// });
+// //监听activite事件， 激活后通过cache的key来判断是否需要更新cache中的静态资源
+// self.addEventListener('activate',function (e) {
+//     console.log('service worker 激活成功');
+//     const cachePromise = caches.keys().then(function (keys) {
+//         return Promise.all(keys.map(function (key) {
+//             if(key !== cacheName) {
+//                 return caches.delete(key);
+//             }
+//         }))
+//     });
+//     e.waitUntil(cachePromise);
+//     return self.clients.claim(); //激活触发Service Worker
+// });
+// //cache存在则使用cache，无cache则fetch服务器端请求资源
+// self.addEventListener('fetch', function (e) {
+//     // console.log('service worker 抓取请求: ' + e.request.url)
+//     e.respondWith(
+//       caches.match(e.request).then(function (cache) {
+//           return cache || fetch(e.request);
+//       }).catch(function (err) {
+//             console.log(err);
+//             return fetch(e.request);
+//       })
+//     );
+// });
